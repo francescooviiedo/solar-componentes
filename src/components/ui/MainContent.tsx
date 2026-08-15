4dee471f22c10f5dfc7340ef35eff9eb700aa325
francesco oviedo <francesco.oviedo@defensoria.mg.def.br>
2026-08-15 08:13:36 -0300

    feat: export BarraFiltroUnificado components, types, and hooks in index.ts

--- src/components/ui/MainContent.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from "react";
-
-export default function MainContent({ children }: Readonly<PropsWithChildren>) {
-  return (
-    <div className="bg-white border-2 border-gray-200 rounded-lg">
-      {children}
-    </div>
-  );
-}
